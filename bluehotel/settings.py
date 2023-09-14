2f6a014b4901143029daa23ed98fd1f32b8c21af
simonepietraroia <simonepietraroia21@gmail.com>
2023-09-14 18:02:00 +0000

    updated allowed hosts

--- bluehotel/settings.py
+++ bluehotel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bluehotel.herokuapp.com']
+ALLOWED_HOSTS = ['bluehotel-31aa453a87a4.herokuapp.com']
 
 
 # Application definition
