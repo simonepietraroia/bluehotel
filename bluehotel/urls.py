cb21d3b72ec06db1185a5bce7cb6813050a749cd
simonepietraroia <simonepietraroia21@gmail.com>
2023-09-14 18:26:02 +0000

    connected base template to url with views

--- bluehotel/urls.py
+++ bluehotel/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from booking.views import get_home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_home_page, name= 'get_home_page')
 ]
