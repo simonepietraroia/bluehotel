c61ff6e24a7eddcf040fea1c185477d6837fdd48
simonepietraroia <simonepietraroia21@gmail.com>
2023-09-15 17:05:11 +0000

    added script code for the button

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-temporary
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("toggle-menu");
+    const menuContainer = document.querySelector(".menu-container");
+    const overlay = document.getElementById("overlay");
+
+    toggleButton.addEventListener("click", function () {
+        menuContainer.style.left = menuContainer.style.left === "0px" ? "-250px" : "0px";
+        overlay.style.width = overlay.style.width === "100%" ? "0" : "100%";
+    });
+
+    overlay.addEventListener("click", function () {
+        menuContainer.style.left = "-250px";
+        overlay.style.width = "0";
+    });
+});
