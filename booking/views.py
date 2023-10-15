72f53a83e3ae9aa5fd475f0d86e9127ec5252201
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-15 16:27:45 +0000

    created index html and made base html the template

--- booking/views.py
+++ booking/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import GuestBooking
-from .forms import GuestBookingForm 
 
 def get_home_page(request):
-    return render(request, '../templates/base.html')
+    return render(request, '../templates/index.html')
 
 def create_guest_booking(request):
-    if request.method == 'POST':
-        form = GuestBookingForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('booking_list')
-    else:
-        form = GuestBookingForm()
-    
-    return render(request, 'booking/create_booking.html', {'form': form})
+    return render(request, '../templates/create_booking.html')
 
