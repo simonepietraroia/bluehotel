f7aff216a589f6b8c0ab93282b8498544a882a5a
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-15 17:09:17 +0000

    updated form in forms.py

--- booking/forms.py
+++ booking/forms.py
@@ -5,3 +5,14 @@ class GuestBookingForm(forms.ModelForm):
     class Meta:
         model = GuestBooking
         fields = ['user', 'email', 'check_in_date', 'check_out_date', 'num_guests', 'room_type']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        check_in_date = cleaned_data.get('check_in_date')
+        check_out_date = cleaned_data.get('check_out_date')
+
+        if check_in_date and check_out_date and check_in_date >= check_out_date:
+            self.add_error('check_in_date', 'Check-in date must be before check-out date')
+
+    def __init__(self, *args, **kwargs):
+        super(GuestBookingForm, self).__init__(*args, **kwargs)
