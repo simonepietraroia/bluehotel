63e877d699e8b267e3802041fcc1c4fcf7d145d3
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-15 17:43:26 +0000

    updated form and views for functionality

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_home_page, name= 'get_home_page'),
-    path('create/', views.create_guest_booking, name='create_booking')
+    path('create/', views.GuestBookingView, name='create_booking')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
 from .models import GuestBooking
+from .forms import GuestBookingForm
 
 def get_home_page(request):
     return render(request, '../templates/index.html')
 
-def create_guest_booking(request):
-    return render(request, '../templates/create_booking.html')
+class GuestBookingView(View):
+    template_name = 'create_booking.html'
+
+    def get(self, request, booking_id=None):
+        if booking_id:
+            booking = get_object_or_404(GuestBooking, pk=booking_id)
+            form = GuestBookingForm(instance=booking)
+        else:
+            form = GuestBookingForm()
+
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, booking_id=None):
+        if booking_id:
+            booking = get_object_or_404(GuestBooking, pk=booking_id)
+            form = GuestBookingForm(request.POST, instance=booking)
+        else:
+            form = GuestBookingForm(request.POST)
+
+        if form.is_valid():
+            booking = form.save()
+            return redirect('booking_confirmation')
+        else:
+            context = {
+                'form': form,
+            }
+            return render(request, self.template_name, context)
 
