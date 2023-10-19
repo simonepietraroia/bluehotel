515ad9931b3a50220cca2bfd2e11f115bae2097e
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-19 17:15:50 +0000

    fixed issue with django version

--- bluehotel/settings.py
+++ bluehotel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bluehotel-31aa453a87a4.herokuapp.com', '8000-simonepietrar-bluehotel-ilt1o9a7zwj.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['bluehotel-31aa453a87a4.herokuapp.com', '8000-simonepietrar-bluehotel-ilt1o9a7zwj.ws-eu105.gitpod.io', '8000-simonepietrar-bluehotel-qt4e8l7frql.ws-eu105.gitpod.io']
 
 
 # Application definition
