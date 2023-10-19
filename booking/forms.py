c5d74735de6ea91ee77277fb562b84a90fd1df4b
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-19 17:50:02 +0000

    connected form to view and html

--- booking/forms.py
+++ booking/forms.py
@@ -6,14 +6,3 @@ class GuestBookingForm(forms.ModelForm):
         model = GuestBooking
         fields = ['user', 'email', 'check_in_date', 'check_out_date', 'num_guests', 'room_type']
 
-    def clean(self):
-        cleaned_data = super().clean()
-        check_in_date = cleaned_data.get('check_in_date')
-        check_out_date = cleaned_data.get('check_out_date')
-
-        if check_in_date and check_out_date and check_in_date >= check_out_date:
-            self.add_error('check_in_date', 'Check-in date must be before check-out date')
-
-    def __init__(self, *args, **kwargs):
-        super(GuestBookingForm, self).__init__(*args, **kwargs)
-
--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_home_page, name= 'get_home_page'),
-    path('create/', views.GuestBookingView.as_view(), name='create_booking')
+    path('create/', views.CreateGuestBooking.as_view(), name='create_booking')
 ]
