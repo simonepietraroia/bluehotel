9c4f0cf8db2377a2abc27a91ca1313a9fce37ff0
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-21 16:37:32 +0000

    added name to model and edited create booking form

--- booking/forms.py
+++ booking/forms.py
@@ -6,13 +6,13 @@ from .models import GuestBooking
 class GuestBookingForm(forms.ModelForm):
     class Meta:
         model = GuestBooking
-        fields = ['user', 'email', 'check_in_date', 'check_out_date', 'num_guests', 'room_type']
+        fields = ['name', 'email', 'check_in_date', 'check_out_date', 'num_guests', 'room_type']
 
     def __init__(self, *args, **kwargs):
         super(GuestBookingForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            'user',
+            'name',
             'email',
             'check_in_date',
             'check_out_date',
--- booking/models.py
+++ booking/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 ROOM_CHOICES = ((0,'1 Bed'), (1, '2 Bed'))
 class GuestBooking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True)
     email = models.EmailField()
     check_in_date = models.DateField()
     check_out_date = models.DateField()
