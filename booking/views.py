740b23f838c30b8dbfb91d5abb25821ba2d5b5b5
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-27 15:02:33 +0000

    created redirect once booking is made and changed the default template of the sign out page

--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import GuestBooking
 from django.views.generic import ListView, CreateView, UpdateView
 from .forms import GuestBookingForm
+from django.urls import reverse
 
 def get_home_page(request):
     return render(request, '../templates/index.html')
@@ -15,11 +16,13 @@ class GuestBookingCreateView(CreateView):
     model = GuestBooking
     template_name = 'create_booking.html'
     form_class = GuestBookingForm
-    success_url = 'get_home_page'  
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('get_home_page')
             
 class GuestBookingUpdateView(UpdateView):
     model = GuestBooking
