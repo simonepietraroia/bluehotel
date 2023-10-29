6eb313904dbbc53cc363f6e98bb2a604142f2855
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-29 16:57:33 +0000

    installed jest and configured it

--- bluehotel/settings.py
+++ bluehotel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bluehotel-31aa453a87a4.herokuapp.com', '8000-simonepietrar-bluehotel-de77pgwyatc.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['bluehotel-31aa453a87a4.herokuapp.com', '8000-simonepietrar-bluehotel-opywfdihifu.ws-eu105.gitpod.io']
 
 
 # Application definition
