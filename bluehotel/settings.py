4a006378d28c7384d792524e915d89ec9ddc1d76
simonepietraroia <simonepietraroia21@gmail.com>
2023-10-30 13:42:09 +0000

    created files for python testing

--- bluehotel/settings.py
+++ bluehotel/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bluehotel-31aa453a87a4.herokuapp.com', '8000-simonepietrar-bluehotel-qx6or057zma.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['bluehotel-31aa453a87a4.herokuapp.com', '8000-simonepietrar-bluehotel-tegbrjmjprh.ws-eu105.gitpod.io']
 
 
 # Application definition
